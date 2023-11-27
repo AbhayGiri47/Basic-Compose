2288057c87ecdbafac4cbc7932c5054e964c99df
Abhay Giri <giriabhay47@gmail.com>
2023-11-27 17:37:23 +0530

    added code for business card practice compose basics

--- app/src/main/java/com/example/happybirthday/ComposeArticle2.kt
+++ app/src/main/java/com/example/happybirthday/ComposeArticle2.kt
@@ -35,38 +35,44 @@ class ComposeArticle2 : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
-                    Column() {
+                    ComposeArticleApp()
 
-                        ComposeImage(
-                            painter = painterResource(id = R.drawable.bg_compose_background),
-                            contentScale = ContentScale.FillWidth
-                        )
-                        ComposeText(
-                            message = stringResource(R.string.article_title),
-                            modifier = Modifier.padding(16.dp),
-                            24.sp,
-                            TextAlign.Start,
-                            FontWeight.Bold
-                        )
-                        ComposeText(
-                            message = stringResource(R.string.article_short_description),
-                            modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 0.dp),
-                            14.sp,
-                            TextAlign.Justify,
-                            FontWeight.Bold
-                        )
-                        ComposeText(
-                            message = stringResource(id = R.string.article_long_description),
-                            modifier = Modifier.padding(16.dp),
-                            14.sp,
-                            TextAlign.Justify,
-                            FontWeight.Bold
-                        )
-
-                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun ComposeArticleApp() {
+    Column() {
+
+        ComposeImage(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentScale = ContentScale.FillWidth
+        )
+        ComposeText(
+            message = stringResource(R.string.article_title),
+            modifier = Modifier.padding(16.dp),
+            24.sp,
+            TextAlign.Start,
+            FontWeight.Bold
+        )
+        ComposeText(
+            message = stringResource(R.string.article_short_description),
+            modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 0.dp),
+            14.sp,
+            TextAlign.Justify,
+            FontWeight.Bold
+        )
+        ComposeText(
+            message = stringResource(id = R.string.article_long_description),
+            modifier = Modifier.padding(16.dp),
+            14.sp,
+            TextAlign.Justify,
+            FontWeight.Bold
+        )
+
     }
 }
 
@@ -85,7 +91,13 @@ fun ComposeText(
     textALign: TextAlign,
     fontweight: FontWeight
 ) {
-    Text(text = message, fontSize = fontSize, modifier = modifier, textAlign = textALign, fontWeight = fontweight)
+    Text(
+        text = message,
+        fontSize = fontSize,
+        modifier = modifier,
+        textAlign = textALign,
+        fontWeight = fontweight
+    )
 }
 
 
@@ -93,34 +105,6 @@ fun ComposeText(
 @Composable
 fun DefaultPreview() {
     HappyBirthdayTheme {
-        Column() {
-
-            ComposeImage(
-                painter = painterResource(id = R.drawable.bg_compose_background),
-                contentScale = ContentScale.FillWidth
-            )
-            ComposeText(
-                message = "Jetpack Compose tutorial",
-                modifier = Modifier.padding(16.dp),
-                24.sp,
-                TextAlign.Start,
-                FontWeight.Normal
-            )
-            ComposeText(
-                message = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-                modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 0.dp),
-                14.sp,
-                TextAlign.Justify,
-                FontWeight.Normal
-            )
-            ComposeText(
-                message = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
-                modifier = Modifier.padding(16.dp),
-                14.sp,
-                TextAlign.Justify,
-                FontWeight.Normal
-            )
-
-        }
+        ComposeArticleApp()
     }
 }
